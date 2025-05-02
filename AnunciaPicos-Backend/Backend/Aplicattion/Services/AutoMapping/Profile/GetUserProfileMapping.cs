451de0d2c9c1232e515b8084d5e8862a56ddb9c6
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-05-02 15:42:55 -0300

    fix: atualizações de observação de perfil de usuários.

--- AnunciaPicos-Backend/Backend/Aplicattion/Services/AutoMapping/Profile/GetUserProfileMapping.cs
+++ AnunciaPicos-Backend/Backend/Aplicattion/Services/AutoMapping/Profile/GetUserProfileMapping.cs
@@ -9,12 +9,14 @@ namespace AnunciaPicos.Backend.Aplicattion.Services.AutoMapping.Profile
         public GetUserProfileMapping()
         {
             CreateMap<UserModel, ResponseGetProfileCommunication>()
+                .ForMember(dest => dest.ImageProfile, opt => opt.MapFrom(src => src.ImageProfile))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.CPF, opt => opt.MapFrom(src => src.CPF))
                 .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone));
 
             CreateMap<UserModel, ResponseGetProfileIdCommunication>()
+                .ForMember(dest => dest.ImageProfile, opt => opt.MapFrom(src => src.ImageProfile))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone));
--- AnunciaPicos-Backend/Shared/Communication/Response/Profile/ResponseGetProfileCommunication.cs
+++ AnunciaPicos-Backend/Shared/Communication/Response/Profile/ResponseGetProfileCommunication.cs
@@ -2,6 +2,7 @@
 {
     public class ResponseGetProfileCommunication
     {
+        public string ImageProfile { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
 
         public string Email { get; set; } = string.Empty;
