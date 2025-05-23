fcf97332757dfd73727e85d847a7e94b60aba8b4
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-05-23 10:57:12 -0300

    feat: implementação do get produtcs de favoritos

--- AnunciaPicos-Backend/Backend/Aplicattion/UseCases/Favorites/Get/IGetFavoriteUseCase.cs
+++ AnunciaPicos-Backend/Backend/Aplicattion/UseCases/Favorites/Get/IGetFavoriteUseCase.cs
@@ -1,9 +1,10 @@
 ﻿using AnunciaPicos.Backend.Infrastructure.Models;
+using AnunciaPicos.Shared.Communication.Response.Product;
 
 namespace AnunciaPicos.Backend.Aplicattion.UseCases.Favorites.Get
 {
     public interface IGetFavoriteUseCase
     {
-        public Task<List<ProductModel>> Execute();
+        public Task<List<ResponseProductGetCommunication>> Execute();
     }
 }
