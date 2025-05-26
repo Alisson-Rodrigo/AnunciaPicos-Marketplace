a6d69ef485e6a552fc8a245406b9720f2b11fb19
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-05-26 14:33:35 -0300

    feat: implementação de data de criação da conta

--- AnunciaPicos-Backend/Backend/Aplicattion/Services/AutoMapping/Profile/GetUserProfileMapping.cs
+++ AnunciaPicos-Backend/Backend/Aplicattion/Services/AutoMapping/Profile/GetUserProfileMapping.cs
@@ -14,13 +14,16 @@ namespace AnunciaPicos.Backend.Aplicattion.Services.AutoMapping.Profile
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.CPF, opt => opt.MapFrom(src => src.CPF))
-                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone));
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+                .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreatedAt));
+
 
             CreateMap<UserModel, ResponseGetProfileIdCommunication>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.ImageProfile, opt => opt.MapFrom(src => src.ImageProfile))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreatedAt))
                 .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone));
         }
     }
